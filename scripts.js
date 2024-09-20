31f61ef68e9d90edc3d8bd8308dbb58731bd0067
DoeChromium <142622054+DoeChromium@users.noreply.github.com>
2024-09-19 22:51:41 -0700

    BETA v1.2

--- scripts.js
+++ scripts.js
@@ -1,6 +1,58 @@
-// $(document).ready(function() {
-//   $('#arrow').click(function() {
-//   $(this).parent().toggleClass('full');
-//    $(this).toggleClass('switch');
-//   });
-// });
+window.onload = () => {
+    homeBTN.setAttribute('id', 'selected')
+}
+
+const ifrm = document.getElementById('main-iframe');
+
+function siteHome() {
+    ifrm.setAttribute('src', 'home.html');
+}
+
+function siteSchedule() {
+    ifrm.setAttribute('src', 'schedule.html');
+}
+
+function siteProjects() {
+    ifrm.setAttribute('src', 'null.html');
+}
+
+function siteAboutUs() {
+    ifrm.setAttribute('src', 'about-us.html');
+}
+
+const homeBTN = document.getElementById('home')
+const scheBTN = document.getElementById('schedule')
+const projBTN = document.getElementById('projects')
+const abtBTN = document.getElementById('about-us')
+
+function clkHome() {
+    homeBTN.setAttribute('id', 'selected');
+
+    scheBTN.setAttribute('id', 'schedule');
+    projBTN.setAttribute('id', 'projects');
+    abtBTN.setAttribute('id', 'about-us');
+}
+
+function clkSche() {
+    scheBTN.setAttribute('id', 'selected');
+
+    homeBTN.setAttribute('id', 'home');
+    projBTN.setAttribute('id', 'projects');
+    abtBTN.setAttribute('id', 'about-us');
+}
+
+function clkProj() {
+    projBTN.setAttribute('id', 'selected');
+
+    scheBTN.setAttribute('id', 'schedule');
+    homeBTN.setAttribute('id', 'home');
+    abtBTN.setAttribute('id', 'about-us');
+}
+
+function clkAbtUs() {
+    abtBTN.setAttribute('id', 'selected');
+
+    scheBTN.setAttribute('id', 'schedule');
+    projBTN.setAttribute('id', 'projects');
+    homeBTN.setAttribute('id', 'home');
+}
