9c2b2445284e631962199a97373da6ced25ddae4
DoeChromium <142622054+DoeChromium@users.noreply.github.com>
2024-10-14 09:44:58 -0700

    BETA 1.4

--- scripts.js
+++ scripts.js
@@ -13,17 +13,27 @@ function siteSchedule() {
 }
 
 function siteProjects() {
-    ifrm.setAttribute('src', 'null.html');
+    ifrm.setAttribute('src', 'projects.html');
 }
 
 function siteAboutUs() {
     ifrm.setAttribute('src', 'about-us.html');
 }
 
+function siteAp() {
+    ifrm.setAttribute('src', 'agents.html');
+}
+
+function siteInfo() {
+    ifrm.setAttribute('src', 'info.html');
+}
+
 const homeBTN = document.getElementById('home')
 const scheBTN = document.getElementById('schedule')
 const projBTN = document.getElementById('projects')
 const abtBTN = document.getElementById('about-us')
+const ap = document.getElementById('agent-p')
+const info = document.getElementById('info')
 
 function clkHome() {
     homeBTN.setAttribute('id', 'selected');
@@ -31,6 +41,8 @@ function clkHome() {
     scheBTN.setAttribute('id', 'schedule');
     projBTN.setAttribute('id', 'projects');
     abtBTN.setAttribute('id', 'about-us');
+    ap.setAttribute('id', 'agent-p')
+    info.setAttribute('id', 'info')
 }
 
 function clkSche() {
@@ -39,6 +51,8 @@ function clkSche() {
     homeBTN.setAttribute('id', 'home');
     projBTN.setAttribute('id', 'projects');
     abtBTN.setAttribute('id', 'about-us');
+    ap.setAttribute('id', 'agent-p')
+    info.setAttribute('id', 'info')
 }
 
 function clkProj() {
@@ -47,6 +61,8 @@ function clkProj() {
     scheBTN.setAttribute('id', 'schedule');
     homeBTN.setAttribute('id', 'home');
     abtBTN.setAttribute('id', 'about-us');
+    ap.setAttribute('id', 'agent-p')
+    info.setAttribute('id', 'info')
 }
 
 function clkAbtUs() {
@@ -55,26 +71,51 @@ function clkAbtUs() {
     scheBTN.setAttribute('id', 'schedule');
     projBTN.setAttribute('id', 'projects');
     homeBTN.setAttribute('id', 'home');
+    ap.setAttribute('id', 'agent-p')
+    info.setAttribute('id', 'info')
 }
+
+function clkAp() {
+    ap.setAttribute('id', 'selected');
+
+    scheBTN.setAttribute('id', 'schedule');
+    projBTN.setAttribute('id', 'projects');
+    homeBTN.setAttribute('id', 'home');
+    abtBTN.setAttribute('id', 'about-us');
+    info.setAttribute('id', 'info')
+}
+
+function clkInfo() {
+    info.setAttribute('id', 'selected');
+
+    scheBTN.setAttribute('id', 'schedule');
+    projBTN.setAttribute('id', 'projects');
+    homeBTN.setAttribute('id', 'home');
+    abtBTN.setAttribute('id', 'about-us');
+    ap.setAttribute('id', 'agent-p')
+}
+
+// end
 
 const width = screen.width;
 const height = screen.height;
 
 const main = document.getElementById('main-iframe')
-const print = document.getElementById('print').innerHTML = width + " x " + height
+const print = document.getElementById('print')
 
-// if (width == "1517") {alert('1517')}
-// if (height == "852") {alert('852')}
+print.innerHTML = width + " x " + height
 
-// document.getElementById('main-header').innerHTML = width + " " + height
+const alt = document.getElementById('main')
 
 if (width == "1517", height == "852") {
     main.style.width = "1308px";
-    main.style.height = "606px"
+    main.style.height = "606px";
+    alt.style.width = "1308px"
 } else {
     if (width == "1920", height == "1080") {
-        main.style.height = "606px"
-        main.style.width = "1760px";
+        main.style.width = "1758px";
+        main.style.height = "793px";
+        alt.style.width = "1758px"
     }
     else {
         alert('unknown screen size!')
@@ -86,9 +127,9 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
     if (confirm('You are on mobile, would you like to visit the mobile-friendly site?')) {
         window.location.replace('mobile.html')
     } else {
-        void(0)
+        void (0)
     }
-    document.getElementById('print').textContent += " | mobile warning!"
+    print.textContent += " | mobile warning!"
 } else {
-    document.getElementById('print').textContent += " | not mobile"
+    print.textContent += " | not mobile"
 }
